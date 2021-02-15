d23d4db1c38474866992a262d6b6fc42bf38d453
IshiiRin <rintaro.ishii0719@gmail.com>
2021-02-16 08:19:46 +0900

    131.5-3 route_auth.go Logout1

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+	"todo_app/app/models"
 	"todo_app/config"
 )
 
@@ -16,6 +17,17 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 	templates.ExecuteTemplate(w, "layout", data)
 }
 
+func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		sess = models.Session{UUID: cookie.Value}
+		if ok, _ := sess.CheckSession(); !ok {
+			err = fmt.Errorf("Invalid session")
+		}
+	}
+	return sess, err
+}
+
 //StartMainServer サーバーを立ち上げる
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
@@ -24,5 +36,6 @@ func StartMainServer() error {
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/todos", index)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
